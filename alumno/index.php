0968f1c86d4fb1b7a8a19b0371b27f306d5a9fc3
Francesc Edo Trias <Francesc Edo Trias>
2016-11-14 17:17:48 +0100

    organizando head

--- alumno/index.php
+++ alumno/index.php
@@ -4,33 +4,28 @@ session_start();
 if(!isset($_SESSION["nombre"])){
   header("location:index.php");
 }
-
 require('../functions.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
 	<title>Alumno</title>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="../js/main.js"></script> <!-- Resource jQuery -->
-	<link href="../themes/redmond/jquery-ui-1.8.16.custom.css" rel="stylesheet" type="text/css" />
-	<link href="../scripts/jtable/themes/lightcolor/blue/jtable.css" rel="stylesheet" type="text/css" />
+	<meta charset="UTF-8">
 	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
 	<link rel="stylesheet" href="../css/reset.css"> <!-- CSS reset -->
 	<link rel="stylesheet" type="text/css" href="../css/menu.css">
-	<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,600,700' rel='stylesheet' type='text/css'>
-    
-    <link href="../css/styleLoginPage.css" type="text/css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" type="text/css" href="../css/styleTableAndMenu.css">
+    <link href="../css/styleLoginPage.css" rel="stylesheet" type="text/css" type="text/css">
+	<link href="../css/styleTableAndMenu.css" rel="stylesheet" type="text/css" >
 	
-	<script src="../scripts/jquery-1.6.4.min.js" type="text/javascript"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="../js/main.js"></script> <!-- Resource jQuery -->
+    
+	<!--JTABLES: start-->
+	<link href="../themes/redmond/jquery-ui-1.8.16.custom.css" rel="stylesheet" type="text/css" />
+	<link href="../scripts/jtable/themes/lightcolor/blue/jtable.css" rel="stylesheet" type="text/css" />
     <script src="../scripts/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
     <script src="../scripts/jtable/jquery.jtable.js" type="text/javascript"></script>
-	<script type="text/javascript" src="../js/modernizr.js"></script>
-	
+	<!--JTABLES: end-->	
 </head>
 <body>
 	
